cf315caea12c1f624deadcbd513c50cb9a37d356
Z50BXH <brad.hardwick@pes-companies.com>
2017-09-12 18:19:10 -0400

    Changed the program src to throw a compile time error

--- src/main/program.java
+++ src/main/program.java
@@ -8,7 +8,7 @@ public class program {
     public static void main(String[] args) {
     	
     	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    	
+    	This is some sample text that will not compile.
     	try {
     		String max;
     		if(args.length>0 && args != null)
